8e6c387fefa1b0f644745325cf37a1b4ac123902
Jacob Zuo <chopinsky@live.com>
2018-07-03 01:13:43 -0500

    adding facial

--- facial_recog/face_recog.py
+++ facial_recog/face_recog.py
@@ -16,5 +16,36 @@ def parse_args():
     return vars(ap.parse_args())
 
 
+def process_image(args):
+    print("info: processing image...")
+    data = pickle.loads(open(args["encodings"], "rb").read())
+
+    image = cv2.imread(args["image"])
+    rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    boxes = face_recognition.face_locations(rgb, model=args["detection_method"])
+    encodings = face_recognition.face_encodings(rgb, boxes)
+
+    names = []
+    for encoding in encodings:
+        matches = face_recognition.compare_faces(data["encodings"], encoding)
+        name = "Unknown"
+
+        if True in matches:
+            indices = [i for (i, b) in enumerate(matches) if b]
+            counts = {}
+
+            for i in indices:
+                name = data["name"][i]
+                counts[name] = counts.get(name, 0) + 1
+
+            name = max(counts, key=counts.get)
+
+        names.append(name)
+
+    return names, boxes
+
+
 if __name__ == '__main__':
     args = parse_args()
+    names, boxes = process_image(args)
